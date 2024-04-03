4f4409c7eaa09873c7c601a7c962e3d44a9d14e8
tiasa20 <89855097+tiasa20@users.noreply.github.com>
2024-04-03 15:58:20 +0530

    Update testRunner.java

--- src/test/java/TestCase/testRunner.java
+++ src/test/java/TestCase/testRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "C:\\Users\\2308397\\eclipse-workspace\\CAS_Project\\IdentifyNewBikes_Hackathon-main\\IdentifyNewBikes_Hackathon-main\\FeatureFiles\\login.feature", glue = "stepDefinition", plugin = { "pretty",
+@CucumberOptions(features = "C:\\Users\\2319871\\eclipse-workspace\\Hackathon2\\FeatureFiles\\login.feature", glue = "stepDefinition", plugin = { "pretty",
 		"html:reports/myCucumberReport.html" })
 public class testRunner {
 
